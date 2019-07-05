28c766ff716c8aad27ce3e6acf347b3005e07d48
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-05 09:54:18 +0100

    Added basic components hierarchy

---
+++ src/components/QuoteAdder.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const QuoteAdder = () => {
+  return <form>QuoteAdder</form>;
+};
+
+export default QuoteAdder;
---
+++ src/components/Quotes.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Quptes = () => {
+  return <section>Some Quote</section>;
+};
+
+export default Quptes;
